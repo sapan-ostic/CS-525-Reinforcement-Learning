50fce9dec55e465f81cfae4b5a46166747b57843
sapanagrawal777 <sapanagrawal777@gmail.com>
2019-10-24 00:10:41 -0400

    epsilon decay updated

--- project3/agent_dqn.py
+++ project3/agent_dqn.py
@@ -6,6 +6,7 @@ from collections import deque
 from collections import namedtuple
 import math
 from matplotlib import pyplot as plt 
+import csv
 
 import os
 import sys
@@ -47,7 +48,7 @@ class Agent_DQN():
 
         self.EPSILON = 0.99
         self.EPS_START = self.EPSILON
-        self.EPS_END = 0.02 
+        self.EPS_END = 0.1
         self.EPS_DECAY = 100000
 
         self.ALPHA = 1e-4
@@ -55,7 +56,7 @@ class Agent_DQN():
         self.actionSpace = [0,1,2,3]
 
         # Parameters for Replay Buffer
-        self.CAPACITY = 10000 # Memory size
+        self.CAPACITY = 40000 # Memory size
         self.memory = deque(maxlen=self.CAPACITY) #namedtuple to be used
         self.batch_size = 32
         self.memCntr = 0 # Total sample stored, len(memory) 
@@ -87,6 +88,7 @@ class Agent_DQN():
             print('loading trained model')
             ###########################
             # YOUR IMPLEMENTATION HERE #
+            policy_net.load_state_dict(torch.load('test'))
             
     def init_game_setting(self):
         """
@@ -140,15 +142,7 @@ class Agent_DQN():
         """
         ###########################
         # YOUR IMPLEMENTATION HERE #
-        # rand = np.random.random()
-        # observation = torch.Tensor(observation).unsqueeze(0)
-        # actions = self.policy_net.forward(observation)
-
-        # if rand < 1 - self.EPSILON:
-        #     action = torch.argmax(actions[0]).item()
-        # else:
-        #     action = np.random.choice(self.actionSpace)
-
+        # observation = observation.transpose(2,0,1)
         device = self.policy_net.device
 
         if np.random.random() < self.EPSILON:
@@ -159,11 +153,6 @@ class Agent_DQN():
             q_vals_v = self.policy_net(state_v)
             _, act_v = torch.max(q_vals_v, dim=1)
             action = int(act_v.item())
-
-        # Update exploration factor
-        self.EPSILON = self.EPS_END + (self.EPS_START - self.EPS_END) *math.exp(-1 * self.steps/self.EPS_DECAY)
-        self.storeEpsilon.append(self.EPSILON)
-        self.steps += 1
 
         ###########################
         return action
@@ -185,53 +174,8 @@ class Agent_DQN():
         next_state_values[done] = 0.0
         next_state_values = next_state_values.detach()
 
-        # print(type(next_state_values))
-        # print(type(self.GAMMA))
-        # print(type(rewards_v))
-
         expected_state_action_values = rewards_v.type(torch.cuda.FloatTensor) + next_state_values * self.GAMMA #+ rewards_v
 
-        # transitions[0][0] = state[1] = 4 images [4x84x84]
-        # transitions = np.array(self.replay_buffer())
-
-        # # Get Q values for State and Action [32x4] 
-        # Qstate = self.policy_net.forward(list(transitions[:,0])).to(self.policy_net.device)
-
-        # # Get Q values for State and Action [32x4]
-        # QNextState = self.target_net.forward(list(transitions[:,2])).to(self.policy_net.device)    
-
-        # # Find the action with max Q values at the next state
-        # maxActions = torch.argmax(QNextState, dim=1).to(self.policy_net.device)
-
-        # rewards = torch.reshape(torch.Tensor(list(transitions[:,3])), (self.batch_size,1)).to(self.policy_net.device)
-
-        # # print('--------Qstate---------')
-        # print(Qstate)
-
-        # # Qtarget.data.copy(Qstate.data)
-        # # Qtarget = torch.tensor.new_tensor(Qstate)
-        # Qtarget = Qstate.clone()    .detach()
-
-        # # Qtarget[:,maxActions] = rewards + self.GAMMA*torch.max(QNextState[1])
-        # temp = torch.reshape(torch.tensor([self.GAMMA*torch.max(QNextState[i]) for i in range(self.batch_size)]), (self.batch_size,1)).to(self.policy_net.device)
-        
-
-        # print(temp)
-        # print(rewards)
-        # for i in range(self.batch_size):
-        #     Qtarget[i,maxActions[i]] = rewards[i] + temp[i]
-
-        # Qtarget[:, maxActions] = rewards + temp
-        # print('--------QNextState---------')
-        # print(QNextState)
-        # print('--------Qtarget---------')
-        # print(Qtarget)
-        # print('--------Qstate---------')
-        # print(Qstate)
-        # print('$$$$$$$$$$$$$$$$$')
-
-
-        # loss = self.policy_net.loss(Qtarget,Qstate).to(self.policy_net.device)
         return nn.MSELoss()(state_action_values,expected_state_action_values)
         
         
@@ -265,6 +209,7 @@ class Agent_DQN():
 
         print('Ready to train model ... ') 
         self.scores = []
+        AvgRewards = []
         t = 1
 
         for self.iEpisode in range(nEpisodes):
@@ -282,6 +227,7 @@ class Agent_DQN():
                     
                 nextState, reward, done, info = self.env.step(action)
                 t+=1
+                # state = state.transpose(2,0,1)
                 nextState = nextState.transpose(2,0,1)
 
                 # Updating memory with new experiences
@@ -301,13 +247,38 @@ class Agent_DQN():
                     print('Updating Target Network . . .')
                     self.target_net.load_state_dict(self.policy_net.state_dict())
 
+                # Update exploration factor
+                self.EPSILON = self.EPS_END + (self.EPS_START - self.EPS_END) *math.exp(-1 * self.steps/self.EPS_DECAY)
+                self.storeEpsilon.append(self.EPSILON)
+                self.steps += 1
+
             self.scores.append(score)
             meanScore = np.mean(self.scores[-100:])
-            
+            AvgRewards.append(meanScore)
+
             print('Episode: ', self.iEpisode, ' score:', score, ' Avg Score:',meanScore,' epsilon: ', self.EPSILON, ' t: ', time.time()-t1, ' loss:', loss.item())
             
-            if self.iEpisode % 500 == 0:
-                torch.save(self.policy_net.state_dict(),'test')
+            if self.iEpisode % 2 == 0:
+                torch.save({
+                    'epoch': self.iEpisode,
+                    'model_state_dict': self.policy_net.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss,
+                    'AvgRewards': AvgRewards
+                }, 'trainData')
+
+            # if self.iEpisode % 500 == 0:
+            #     torch.save(self.policy_net.state_dict(),'test')
 
         print('======== Complete ========')
-        # self.device = torch.device('cuda:0')
+        torch.save({
+            'epoch': self.iEpisode,
+            'model_state_dict': self.policy_net.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+            'AvgRewards': AvgRewards
+        }, 'trainData')
+
+        with open('Rewards.csv', mode='w') as dataFile:
+            rewardwriter = csv.writer(dataFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            rewardwriter.writerow(AvgRewards)
